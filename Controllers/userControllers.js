44e377547fd144c26d2f6e5d053bec447fb2ecc7
Ansuhan <anushan559@gmail.com>
2024-05-10 13:59:43 +0530

    completed user route

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -2,21 +2,41 @@ const User=require("../Models/user");
 
 
 exports.getUserbyId=(req, res, next, id)=>{
+    console.log("ID------1-------",id)
     User.findById(id).exec((err,user)=>{
         if(err || !user){
             return res.json({
                 error:"No user found"
             })
         }
+        console.log("ID-------------",user)
         req.profile=user;
     })
     next();
 }
 
 exports.getUser=(req,res)=>{
+   console.log("ID------2-------")
     req.profile.salt=undefined;
     req.profile.encry_password=undefined;
     req.profile.createdAt=undefined;
     req.profile.updatedAt=undefined;
     return res.json(req.profile)
+}
+
+exports.updateUser=(req,res)=>{
+    User.findByIdAndUpdate({_id:req.profile._id},
+                            {$set:req.body},
+                            {new:true,useFindAndModify:false},
+                                (err,user)=>{
+                                    if(err || !user){
+                                        return res.json({
+                                            error:"Error in update"
+                                        })
+                                    }
+                                    user.salt=undefined;
+                                    user.encry_password =undefined;
+                                    res.json(user)
+                                }
+                            )
 }
--- Routers/user.js
+++ Routers/user.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
 
-const {getUserbyId,getUser}=require("../Controllers/userControllers")
+const {getUserbyId,getUser,updateUser}=require("../Controllers/userControllers")
 const {isSignedIn,isAutheticate,isAdmin}=require("../Controllers/authController")
 
 
 router.param("userId",getUserbyId)
 
 router.get("/user/:userId",isSignedIn,isAutheticate,getUser)
+router.put("/user/:userId",isSignedIn,isAutheticate,updateUser)
 module.exports=router
