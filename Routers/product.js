7eb53214dc14488d8df57c1cee5c2f29a52283a8
Ansuhan <anushan559@gmail.com>
2024-05-26 14:03:15 +0530

    Product contoller

--- Routers/product.js
+++ Routers/product.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router=express.Router();
-const {getproductbyId,createProduct}=require("../Controllers/productController")
+const {getproductbyId,createProduct,getAllProducts,getAllUniqueCategories,getProduct,photo,updateProduct,deleteProduct}=require("../Controllers/productController")
 const {isAdmin,isAutheticate,isSignedIn}=require("../Controllers/authController")
 const {getUserbyId}=require("../Controllers/userControllers")
 
@@ -10,7 +10,28 @@ router.param("productid",getproductbyId)
 
 router.post("/product/create/:userId",isSignedIn,isAutheticate,isAdmin,createProduct)
 
+router.get("/product/:productid",getProduct);
+router.get("/product/photo/:productid", photo);
 
+router.delete(
+    "/product/:productid/:userId",
+    isSignedIn,
+    isAutheticate,
+    isAdmin,
+    deleteProduct
+  );
+  
+  //update route
+  router.put(
+    "/product/:productid/:userId",
+    isSignedIn,
+    isAutheticate,
+    isAdmin,
+    updateProduct
+  );
 
+  router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
 
 module.exports=router
